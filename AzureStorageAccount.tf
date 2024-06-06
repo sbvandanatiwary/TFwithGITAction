e8d60e5f17c5e068754a16dbc1186e67a078437d
sbvandanatiwary <sbvandana.tiwary@gmail.com>
2024-06-06 22:56:48 +0530

    Update AzureStorageAccount.tf

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -5,14 +5,14 @@ resource "azurerm_resource_group" "demo" {
 
 ##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "satestant000012"
+  name                     = "ckcsatestant000012"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
   tags = {
-    video = "azure"
-    channel = "CloudQuickLabs"
+    tcloud = "azure"
+    ops = "tf"
   }
 }
