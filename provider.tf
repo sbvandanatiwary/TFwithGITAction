0e9f4f8fa0fa72028a771371fc807ab00a4698a6
sbvandanatiwary <sbvandana.tiwary@gmail.com>
2024-06-06 19:51:26 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "SabreADI"
+
+    workspaces {
+      name = "SabreAditestws"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
