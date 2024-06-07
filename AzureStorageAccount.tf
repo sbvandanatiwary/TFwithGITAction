06e21f3a08cb1875e5e35a61e67f1387c8b117f0
My Name <myemail@example.com>
2024-06-07 11:02:51 +0530

    change done on main branch for github actions

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "demo" {
 
 ##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "ckcsatestant000012"
+  name                     = "ccsatestant000012"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
